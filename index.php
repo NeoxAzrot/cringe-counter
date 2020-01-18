2ef3f7ad47d2dda13ed3a53b8b03d4a7b23dc826
NeoxAzrot1 <sami.lepays@gmail.com>
2020-01-18 22:56:15 +0100

    Goal

--- index.php
+++ index.php
@@ -18,7 +18,8 @@
         ?>
         <h1>Cringe.</h1>
         <p class="countdown"></p>
-        <p class="counter"><?php echo $pages_vues ?></p>
+        <p class="counter"><?php echo $pages_vues ?><span>/ 1000</span></p>
+
         <footer>
             <p>Press « space » or touch the screen.</p>
         </footer>
